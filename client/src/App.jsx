10d07aa8837f1aba34f9bde64c0a05019392b82e
raihannurhidayat <grandrx041@gmail.com>
2024-09-01 19:45:36 +0700

    last commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SignUpPage from './pages/auth/signup/SignUpPage'
-import LoginPage from './pages/auth/login/LoginPage'
-import HomePage from './pages/home/Home'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import HomePage from "./pages/home/Home";
+import Sidebar from "./components/common/Sidebar";
+import NotificationPage from "./pages/notification/NotificationPage";
+import RightPanel from "./components/common/RightPanel";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='flex max-w-6xl mx-auto'>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} /> 
-        <Route path='/login' element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
